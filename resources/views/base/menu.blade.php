94a2bdd94d0f014455b0e098d462c84d8cc5c0a6
dudurb17 <eduardo.robettibedin@gmail.com>
2023-09-08 19:13:28 -0300

    antes q de merda

--- resources/views/base/menu.blade.php
+++ resources/views/base/menu.blade.php
@@ -60,20 +60,6 @@
                             d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
                 </button>
-
-
-
-                <!--
-              Dropdown menu, show/hide based on menu state.
-
-              Entering: "transition ease-out duration-100"
-                From: "transform opacity-0 scale-95"
-                To: "transform opacity-100 scale-100"
-              Leaving: "transition ease-in duration-75"
-                From: "transform opacity-100 scale-100"
-                To: "transform opacity-0 scale-95"
-            -->
-
             </div>
         </div>
     </div>
