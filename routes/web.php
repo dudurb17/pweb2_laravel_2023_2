91ea2665560daa851e1e837f524ccf1f5c0fe672
dudurb17 <eduardo.robettibedin@gmail.com>
2023-10-27 20:30:50 -0300

    Co-authored-by: Lucas Jandrey <LucasJandrey5@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-//importar o arquivo do controlador
+use Inertia\Inertia;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\funcionarioController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
-})->name("home");
+})->name("home");;
 
-Route::get('/funcionario',
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/funcionario',
     [funcionarioController::class, 'listFuncionario'])->name('funcionario.listFuncionario');
 
 Route::get('/funcionario/create',
@@ -32,7 +53,7 @@ Route::get('/funcionario/destroy/{id}',
 
 Route::post('/funcionario/search',
     [funcionarioController::class, 'search'])->name('funcionario.search');
-    
+
 
 //chamar uma função do controlador
 Route::get('/usuario', [UsuarioController::class, 'index']);
@@ -116,3 +137,6 @@ Route::get('/produto/edit/{id}',
 
 Route::post('/produto/update/{id}',
     [ProdutoController::class, 'update'])->name('produto.update');
+});
+
+require __DIR__.'/auth.php';
